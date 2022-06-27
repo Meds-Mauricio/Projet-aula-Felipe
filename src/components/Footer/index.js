5aeb1f07e373eb058617e0f50bbbaa5a35eb12e1
Mauricio Edson de Souza <mauricioesdonsouza@gmail.com>
2022-06-27 15:51:24 -0300

    final-configuração-de-pages

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,27 +5,24 @@ import { FaFacebookSquare } from 'react-icons/fa';
 
 export default function Footer() {
     return (
-            <section className={style.container}>
-                <div className={style.rodape} >
-                    <div>
-                        <h1>Contatos</h1>
-                        <p>loja: (+55) 11 98262-0594</p>
-                        <p>Reclamação: (+55) 11 93023-4589</p>
-                    </div>
-                    <div className={style.horario}>
-                        <h1>Horário de Atendimento</h1>
-                        <p>Segunda à Sábado das 8h às 20h</p>
-                        <p>Exceto Feriados</p>
-                    </div>
-
+        <section className={style.container}>
+            <div className={style.rodape} >
+                <div>
+                    <h1>Contatos</h1>
+                    <p>loja: (+55) 11 98262-0594</p>
+                    <p>Reclamação: (+55) 11 93023-4589</p>
                 </div>
-                <div className={style.redesSociais}>
-                    <h2>Redes Socias</h2>
-                    <div className={style.links}>
-                        <a className={style.whatsapp} href="https://web.whatsapp.com/send?phone=5511982620594" target="_blank"><FaWhatsappSquare /></a>
-                         <a className={style.face} href="https://www.facebook.com/mauricio.edson.1" target="_blank"><FaFacebookSquare /></a>
-                    </div>
+                <div className={style.horario}>
+                    <h1>Horário de Atendimento</h1>
+                    <p>Segunda à Sábado das 8h às 20h</p>
+                    <p>Exceto Feriados</p>
                 </div>
-            </section>
+                <div className={style.links}>
+                <h2>Redes Socias</h2>
+                    <a className={style.whatsapp} href="https://web.whatsapp.com/send?phone=5511982620594" target="_blank"><FaWhatsappSquare /></a>
+                    <a className={style.face} href="https://www.facebook.com/mauricio.edson.1" target="_blank"><FaFacebookSquare /></a>
+                </div>
+            </div>
+        </section>
     )
 }
